9e87ff3a6d05814ae0f7a8f5c87eacf4733fab7e
Kunal Bagaria <kb24x7@gmail.com>
2021-09-21 13:15:08 +0530

    Added wav files support

--- src/play_sound.rs
+++ src/play_sound.rs
@@ -1,10 +1,18 @@
 pub mod sound {
     use std::thread;
     use play;
+    extern crate ears;
+    use ears::{ Sound, AudioController };
     pub fn play_sound(name: String) {
         let file_name = format!("{}", name);
         thread::spawn(move|| {
-            play::play(&file_name[..]).unwrap();
+            if file_name.ends_with(".mp3") {
+                play::play(&file_name[..]).unwrap();
+            } else {
+                let mut sound = Sound::new(&file_name[..]).unwrap();
+                sound.play();
+                while sound.is_playing() {};
+            }
         });
     }
 }
