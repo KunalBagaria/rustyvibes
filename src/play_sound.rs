742300a93177fcdb641cae831d9a0793f752bf3b
Kunal Bagaria <kb24x7@gmail.com>
2021-10-04 17:24:53 +0530

    Use sink

--- src/play_sound.rs
+++ src/play_sound.rs
@@ -2,16 +2,19 @@ pub mod sound {
     use std::thread;
     use std::fs::File;
     use std::io::BufReader;
-    use rodio::{Decoder, OutputStream, source::Source};
+    use rodio::{ source::Source, Decoder, OutputStream };
 
     pub fn play_sound(name: String) {
         let file_name = format!("{}", name);
         thread::spawn(move|| {
             let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-            let file = BufReader::new(File::open(&file_name[..]).unwrap());
-            let source = Decoder::new(file).unwrap();
-            drop(stream_handle.play_raw(source.convert_samples()));
-            std::thread::sleep(std::time::Duration::from_millis(700));
+            let source = {
+                let file = BufReader::new(File::open(&file_name[..]).unwrap());
+                Decoder::new(file).unwrap().buffered()
+            };
+            let sink = rodio::Sink::try_new(&stream_handle).unwrap();
+            sink.append(source);
+            sink.sleep_until_end();
         });
     }
 }
