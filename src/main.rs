5b75aecb37e6baa8037fe15a19673204ad449ae3
Kunal Bagaria <kb24x7@gmail.com>
2021-10-08 23:18:53 +0530

    Testing

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,22 @@ mod listen;
 mod keycode;
 mod play_sound;
 
-// use listen::rustyvibes;
+use listen::rustyvibes;
+use fltk::{ app, prelude::*, window::Window };
+use std::thread;
+
+fn gui() -> Result<(), Box<dyn std::error::Error>> {
+    let app = app::App::default();
+    let mut wind = Window::new(100, 100, 400, 600, "Rustyvibes");
+    wind.end();
+    wind.show();
+    app.run().unwrap();
+    Ok(())
+}
 
 fn main() {
-    println!("Hello World")
+    thread::spawn(move || {
+        // rustyvibes::start_listening("/Users/kunalbagaria/Downloads/Soundpacks/nk-cream".to_string());
+    });
+    drop(gui());
 }
