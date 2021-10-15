da3b99bed6ebe8d6e25f82c8c354d6506c618bf3
Kunal Bagaria <kb24x7@gmail.com>
2021-10-15 13:37:02 +0530

    Added default soundpacks

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use fltk::{ app, prelude::*, window::Window };
 use std::thread;
 use std::process::{ Command };
+use std::env;
+use std::fs;
 
 fn gui() -> Result<(), Box<dyn std::error::Error>> {
     let app = app::App::default();
@@ -11,12 +13,38 @@ fn gui() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+fn rustyvibes_path() -> Result<String, Box<dyn std::error::Error>> {
+    let mut path = env::current_exe()?;
+    path.pop();
+    path.pop();
+    path.push("Resources/resources/rustyvibes");
+    Ok(path.to_str().unwrap().to_string())
+}
+
+fn soundpacks_path() -> Result<String, Box<dyn std::error::Error>> {
+    let mut path = env::current_exe()?;
+    path.pop();
+    path.pop();
+    path.push("Resources/resources/soundpacks");
+    Ok(path.to_str().unwrap().to_string())
+}
+
 fn main() {
-    thread::spawn(|| {
-        drop(Command::new("rustyvibes")
-            .arg("/Users/kunalbagaria/Downloads/Soundpacks/nk-cream")
-            .spawn()
-            .expect("Failed to start echo process"));
-    });
-    drop(gui());
+    let soundpacks_path = soundpacks_path().unwrap();
+    let paths = fs::read_dir(soundpacks_path).unwrap();
+
+    for (i, f) in paths.enumerate() {
+        if i == 8 {
+            thread::spawn(|| {
+                let rustyvibes_path = rustyvibes_path().unwrap();
+                let soundpack_path = f.unwrap().path().display().to_string();
+                println!("{}", rustyvibes_path);
+                drop(Command::new(rustyvibes_path)
+                    .arg(soundpack_path)
+                    .spawn()
+                    .expect("Failed to start echo process"));
+            });
+            drop(gui());
+        }
+    }
 }
